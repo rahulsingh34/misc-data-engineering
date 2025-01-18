58d61f3fead75fdfe88fe0a4308cc613b3215db6
Rahul Singh <singh434rahul@gmail.com>
2025-01-17 19:51:12 -0500

    scd table

--- dimensions/tables.sql
+++ dimensions/tables.sql
@@ -7,3 +7,13 @@ CREATE TABLE actors (
 	year INTEGER,
 	PRIMARY KEY (actorid, year)
 )
+
+CREATE TABLE actors_history_scd (
+	actorid TEXT,
+	quality_class quality_class,
+	is_active BOOLEAN,
+	start_year INTEGER,
+	end_year INTEGER,
+	year INTEGER,
+	PRIMARY KEY(actorid, start_year)
+)
