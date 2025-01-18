af75193c9a2732628d66a157fc1ddd43c7bad18b
Rahul Singh <singh434rahul@gmail.com>
2025-01-17 20:48:14 -0500

    actors scd type

--- dimensions/datatypes.sql
+++ dimensions/datatypes.sql
@@ -16,3 +16,10 @@ CREATE TYPE film_year AS (
 	year INTEGER,
 	films films[]
 )
+
+CREATE TYPE actors_scd_type AS (
+	quality_class quality_class,
+	is_active BOOLEAN,
+	start_year INTEGER,
+	end_year INTEGER
+)
