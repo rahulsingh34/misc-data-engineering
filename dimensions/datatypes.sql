754f15654d028404403223ab528f651b1c7de0da
Rahul Singh <singh434rahul@gmail.com>
2025-01-17 19:50:06 -0500

    datatypes

---
+++ dimensions/datatypes.sql
@@ -0,0 +1,18 @@
+CREATE TYPE films AS (
+	film TEXT,
+	votes INTEGER,
+	rating REAL,
+	filmid TEXT
+)
+
+CREATE TYPE quality_class AS ENUM (
+	'star',
+	'good',
+	'average',
+	'bad'
+)
+
+CREATE TYPE film_year AS (
+	year INTEGER,
+	films films[]
+)
